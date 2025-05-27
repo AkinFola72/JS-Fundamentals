0ee8eafd1df2335e95debdab03ca8549f3a44f19
Akinrolabu Paul <akinrolabupaul@gmail.com>
2025-05-27 18:24:35 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -4,7 +4,7 @@ if (args <= 0){
   console.log('No Argument');
 }
 else if (args == 1){
-  console.log('Best Argument found');
+  console.log('Argument found');
 }
 else {
-  console.log('Best School Argument found');}
+  console.log('Arguments found');}

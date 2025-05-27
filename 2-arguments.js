41d5345f15cd11103854c48ac039be16df88bc74
Akinrolabu Paul <akinrolabupaul@gmail.com>
2025-05-27 18:20:31 +0100

    Create 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+let args = 2
+
+if (args <= 0){
+  console.log('No Argument');
+}
+else if (args == 1){
+  console.log('Best Argument found');
+}
+else {
+  console.log('Best School Argument found');}

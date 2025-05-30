3f1a62b042aab1d7290a5f7e5760584e48b7e2d8
Akinrolabu Paul <akinrolabupaul@gmail.com>
2025-05-30 22:54:04 +0100

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,3 +3,11 @@ const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < messages.length; i++) {
   console.log(messages[i]);
 }
+const message = 'C is fun';
+
+for (let i = 0; i < 6; i++) {
+  console.log(message);
+}
+console.log('C is fun');
+
+// intentionally left blank
